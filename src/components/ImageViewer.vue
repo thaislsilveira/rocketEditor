653e9a8a8d9483bf6ef9af4cb92d801142119b30
Thais Silveira <thais_lsilveira@hotmail.com>
2020-08-22 17:47:20 -0300

    first commit

---
+++ src/components/ImageViewer.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="h-64 rounded-sm bg-brand-black2">
+    <label v-if="!state.hasImage" for="file" class="flex flex-col items-center justify-center w-full h-full cursor-pointer">
+      <input @change="handleUpload" type="file" id="file" class="hidden">
+      <span class="text-6xl"> 😍 </span>
+      <span class="text-2xl font-bold text-center text-white">
+        Clique aqui para fazer o <br>
+        upload da imagem.
+      </span>
+    </label>
+    <div v-else class="flex items-center justify-center w-full h-full p-8">
+      <img  class="w-full" :src="state.image" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+import { reactive, computed } from 'vue';
+
+export default {
+  setup() {
+    const state = reactive({
+      image: null,
+      hasImage: computed(() => !!state.image),
+    })
+
+
+    const handleUpload = (event) => {
+      const [file] = event.target.files;
+      const reader = new FileReader();
+
+      reader.onloadend = () => {
+        state.image = reader.result;
+      }
+
+      reader.readAsDataURL(file);
+    }
+
+    return { state, handleUpload };
+  }
+}
+</script>
---
+++ src/components/Toolbar.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="py-5 rounded-sm bg-brand-black2">
+    <div class="p-5">
+      <h4 class="text-xl font-bold text-white">
+        Saturação
+      </h4>
+      <p class="-mt-1 text-sm text-white">O quão viva a imagem será</p>
+      <custom-range class="mt-4" />
+    </div>
+    <div class="p-5">
+       <h4 class="text-xl font-bold text-white">
+        Desfoque
+      </h4>
+      <p class="-mt-1 text-sm text-white">O quão nítida a imagem será</p>
+      <custom-range class="mt-4"  />
+    </div>
+    <div class="p-5">
+       <h4 class="text-xl font-bold text-white">
+        Brilho
+      </h4>
+      <p class="-mt-1 text-sm text-white">O quão clara a imagem será</p>
+      <custom-range class="mt-4"  />
+    </div>
+    <div class="p-5">
+       <h4 class="text-xl font-bold text-white">
+        P&B
+      </h4>
+      <p class="-mt-1 text-sm text-white">O quão preto/branco a imagem será</p>
+      <custom-range class="mt-4"  />
+    </div>
+    <div class="p-5">
+       <h4 class="text-xl font-bold text-white">
+        Contraste
+      </h4>
+      <p class="-mt-1 text-sm text-white">O quão definido das cores da imagem será</p>
+      <custom-range class="mt-4"  />
+    </div>
+    <div class="p-5">
+      <button class="w-full py-4 text-2xl font-medium text-white uppercase rounded-sm bg-brand-main">
+        baixar imagem
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import CustomRange from './CustomRange.vue';
+export default {
+  components: {
+    CustomRange,
+  }
+}
+</script>
+
+<style>
+
+</style>
